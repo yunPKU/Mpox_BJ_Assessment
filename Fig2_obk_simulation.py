df8d799ccd797d075a2f1e5af77693320955014a
yunPKU <46952732+yunPKU@users.noreply.github.com>
2023-10-23 08:56:53 +0800

    Update Fig2_obk_simulation.py

--- Fig2_obk_simulation.py
+++ Fig2_obk_simulation.py
@@ -74,14 +74,16 @@ def obk_simu(iniEPI_file,tmpKt,Rt_srs,mean_GT, sd_GT,simuK):
     initialsrs = pd.read_csv(iniEPI_file)['IncData'].values  
     Epi_size_July = np.zeros(simuK);
     Epi_size_Aug  = np.zeros(simuK);
-    simLen = 92;
+    Epi_size_Sep = np.zeros(simuK);
+    simLen = 122;
 
     
     MaxInfctPrd = int(mean_GT+3*sd_GT)+1
 
     July_idx = 62;
     Aug_idx = 92;
-    simu_result = np.zeros((len(Rt_srs)+1,7));
+    Sep_idx = 122
+    simu_result = np.zeros((len(Rt_srs)+1,10));
 
     for rt_id in range(len(Rt_srs)):
         simu_result[rt_id,0] = 37
@@ -95,22 +97,24 @@ def obk_simu(iniEPI_file,tmpKt,Rt_srs,mean_GT, sd_GT,simuK):
             
             Epi_size_July[i] = np.sum(EpiSimuData[:July_idx]);
             Epi_size_Aug[i]  = np.sum(EpiSimuData[:Aug_idx]);
-        
+            Epi_size_Sep[i]  = np.sum(EpiSimuData[:Sep_idx]);
 
         # get the results
         mb_size_July = np.median(Epi_size_July)
         mb_size_Aug = np.median(Epi_size_Aug)
+        mb_size_Sep = np.median(Epi_size_Sep)
         
         # output the result
         simu_result[rt_id,1:4] = np.percentile(Epi_size_July,[50, 25, 75])
-        simu_result[rt_id,4:] = np.percentile(Epi_size_Aug,[50, 25, 75])
-    simu_result[-1,:] = np.array([21,62,62,62,92,92,92]);
+        simu_result[rt_id,4:7] = np.percentile(Epi_size_Aug,[50, 25, 75])
+        simu_result[rt_id,7:] = np.percentile(Epi_size_Sep,[50, 25, 75])
+    simu_result[-1,:] = np.array([21,62,62,62,92,92,92,122,122,122]);
 
     acc_case = np.cumsum(initialsrs);
     rpt_date = np.arange(0,len(initialsrs));
 
-    late_case = np.array([45+81,45+81+54])
-    late_date = np.array([62,92])
+    late_case = np.array([45+81,45+81+54,45+81+54+42])
+    late_date = np.array([62,92,122])
 
     acc_case = np.append(acc_case,late_case);
     rpt_date = np.append(rpt_date,late_date);
@@ -127,26 +131,36 @@ plt.style.use('ggplot')
 lw = 2.5
 
 k_srs = [1,2,4,0.5]
-simuK = 100  # times of simulation
+simuK = 50000  # times of simulation
 mean_GT, sd_GT = 8.5, 5.0; # USA data
 # mean_GT, sd_GT= 5.6, 1.5;  # multiple countries
 
 # the csv file of the initial incidence data 
 iniEPI_file = '/Users/BJ-mpx.csv'
 
-fig,axes = plt.subplots(2,2,figsize=(16,9),dpi = 500,sharex=True,sharey = False)
+
+# simulation with the k_srs
+cmp_re = []
+for k_id in range(len(k_srs)):
+    tmpK = k_srs[k_id];
+    cmp_re += [obk_simu(iniEPI_file,tmpK,Rt_srs,mean_GT, sd_GT,simuK)];
+
+
+# plot the results
+fig,axes = plt.subplots(2,2,figsize=(16,10),dpi = 500,sharex=True,sharey = False)
 for k_id in range(len(k_srs)):
     tmpAx = axes[int((k_id-k_id%2)/2),k_id%2];
     tmpK = k_srs[k_id];
-    acc_case,rpt_date,simu_result = obk_simu(iniEPI_file,tmpK,Rt_srs,mean_GT, sd_GT,simuK);    
+    acc_case,rpt_date,simu_result = cmp_re[k_id];    
     tmpAx.plot(rpt_date,acc_case,c='k',linewidth = 1.5*lw);
     for rt_id in range(len(Rt_srs)):
-        tmpAx.plot(simu_result[-1,[0,1,4]],simu_result[rt_id,[0,1,4]],linewidth = lw);
-    tmpAx.set_xticks([0,21,62,92])
-    tmpAx.set_yticks([50,150,250])
+        tmpAx.plot(simu_result[-1,[0,1,4,7]],simu_result[rt_id,[0,1,4,7]],linewidth = lw);
+    tmpAx.set_xticks([0,21,62,92,122])
+    if k_id<3:
+        tmpAx.set_yticks([100,200,300,400])
+    else:
+        tmpAx.set_yticks([100,200,300])
     if k_id%2 == 1:
         tmpAx.yaxis.set_label_position("right")
         tmpAx.yaxis.tick_right()
 plt.subplots_adjust(hspace = 0.05,wspace = 0.03)
-
-
